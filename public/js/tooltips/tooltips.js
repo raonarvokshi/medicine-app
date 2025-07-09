fd38f3987ff864ad185abd05b26e4b21f1da3f32
raonarvokshi <raonar.vokshi@gmail.com>
2025-07-09 12:29:16 +0200

    Fixed sidebar

--- public/js/tooltips/tooltips.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-        new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
